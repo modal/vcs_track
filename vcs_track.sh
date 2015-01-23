06f90a766c8da837c90b983ae852a4e265f3f52d
Tim Dahlin <duhlynn@gmail.com>
2015-01-23 10:02:21 -0600

    Create bash shell version
    This should generate the same output as the python script

--- vcs_track.sh
+++ vcs_track.sh
@@ -1,9 +1,22 @@
+#!/bin/bash
 
-#TODO WORK in progress
+##############################################################################
+f="vcs_track.h"
+echo Creating vcs_track.h
+hash=$(git rev-parse HEAD)
+date=$(git show -s --format=%ci)
 
+echo \#ifndef __VCS_TRACK_H__ > $f
+echo \#define __VCS_TRACK_H__ >> $f
+echo \/\/THIS FILE IS AUTO GENERATED >> $f
+echo \/\/DO NOT TRACK THIS FILE WITH THE VCS >> $f
+echo \#define VCS_TRACK_DATE \"$hash\" >> $f
+echo \#define VCS_TRACK_HASH \"$date\" >> $f
+
+echo \#endif >> $f
+
+##############################################################################
+#Future
 #Get SW commit data
-
 #Get HW commit data
-
-#Get HW/IP commit data  //Recursive?
-
+#Get HW/IP commit data  ==> Recursively and generate a super string?
